9436813efbeb8f82acd96df5affc5558bd86e036
mahersalhani <maheryos5@gmail.com>
2023-12-04 00:04:34 +0300

    finish arrays

---
+++ LinkedLists/src/Album.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    ArrayList<Song> songs = new ArrayList<Song>();
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+    }
+
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        System.out.println("The song " + title + " is already in this album");
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song song : songs) {
+            if (song.getTitle().equals(title)) {
+                return song;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if (index >= 0 && index <= songs.size()) {
+            playList.add(songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song song = findSong(title);
+        if (song != null) {
+            playList.add(song);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+}
+
+class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
